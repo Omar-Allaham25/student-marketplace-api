ee8659f8e59f13b3a7853f16fbcae9fc39e465ca
omar Allaham <omarlaham246@gmail.com>
2026-08-22 19:21:39 +0300

    feat: implement socket authentication; add JWT verification for secure connections

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -2,6 +2,7 @@ import { Server as HttpServer } from "http";
 import { Server, Socket } from "socket.io";
 import { updateMessageReadStatus } from "./models/messageModel";
 import { AppError } from "./utils/appError";
+import  jwt from "jsonwebtoken";
 
 let io: Server;
 
@@ -14,14 +15,23 @@ export const initSocket = (httpServer: HttpServer) => {
     },
   });
 
+  io.use((socket, next) => {
+    try {
+      const token = socket.handshake.auth.token;
+      if(!token){
+        throw new AppError("Socket authentication failed", 401);
+      }
+        const decoded=jwt.verify(token,process.env.SECRET_KEY as string);
+        socket.data.user=decoded;
+        next();
+    } catch (err) {
+      next(new AppError("Socket authentication failed", 401));
+    }
+  });
   io.on("connection", (socket: Socket) => {
-    console.log(`Socket connected: ${socket.id}`);
-
-    socket.on("join_user_room", (userId: string) => {
-      socket.join(`user_${userId}`);
-      console.log(`User ${userId} joined their personal room`);
-    });
-
+    const userId=socket.data.user?.userId;
+    socket.join(`user_${userId}`);
+    console.log(`Securely connected: User ${userId} on socket ${socket.id}`);
     socket.on("join_conversation", (conversationId: string) => {
       socket.join(`chat_${conversationId}`);
       console.log(
